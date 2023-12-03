2edc2ed37488d77b72601706b59568414ef42fd1
ilia-khristoforov <ilia.khristoforov@skoltech.ru>
2023-12-03 22:57:01 +0300

    added gitignore

--- meshing.sh
+++ meshing.sh
@@ -1,4 +1,4 @@
-snappyHexMeshConfig -clearBoundary -insidePoint '(0.07 0.07 0.0027)' -inletRegions '(inlet)' -outletRegions '(outlet)'
+snappyHexMeshConfig -clearBoundary -nCells '(200 190 20)' -insidePoint '(0.07 0.07 0.0027)' -inletRegions '(inlet)' -outletRegions '(outlet)'
 
 blockMesh
 decomposePar
